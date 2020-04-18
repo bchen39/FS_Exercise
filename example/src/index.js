9419bd897421d8c5954fb30422fecfedcd575158
bchen39 <yubangbenchen@berkeley.edu>
2020-04-18 09:15:53 -0700

    unicafe

---
+++ example/src/index.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = ({onClick, text}) => (
+    <button onClick={onClick}>
+      {text}
+    </button>
+)
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const App = (props) => {
+  const [value, setValue] = useState(10)
+
+  const hello = (who) => {
+    const handler = () => {
+      console.log('hello', who)
+    }
+    return handler
+  }
+
+  return (
+    <div>
+      {value}
+      <button onClick={hello('world')}>button</button>
+      <button onClick={hello('react')}>button</button>
+      <button onClick={hello('function')}>button</button>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App/>, 
+  document.getElementById('root')
+)
